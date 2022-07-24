c77cad6202c6c9a718f38c2118a3dac1bc004562
Swarnim Shrivastava <swarnimshrivastava@Swarnims-MacBook-Air.local>
2022-07-24 14:24:08 +0530

    wait_for_db

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django command to waut for the database to be available
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args,**options):
+        pass
